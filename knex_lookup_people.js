bbc3fdc96660c3d88c64d3611ccf3e0e845b7605
Ahmed Al-Hashimi <Alhashimi346@gmail.com>
2017-04-25 21:30:11 +0000

    added knex_lookup.js and use knex instead of pg

---
+++ knex_lookup_people.js
@@ -0,0 +1,29 @@
+const myArgs = process.argv[2];
+const settings = require("./settings"); // settings.json
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+  }
+});
+
+function getPeople (name, done) {
+  knex.select("*")
+  .from("famous_people")
+  .where("first_name", "=", name )
+  .then(function(rows){
+    done(rows);
+  });
+  knex.destroy();
+}
+function showPeople (rows) {
+  rows.forEach(function(people) {
+  console.log(`found ${people.id} person by the name ${myArgs} \n - ${people.id}: ${people.first_name} ${people.last_name} born in ${people.birthdate} `);
+  });
+}
+getPeople(myArgs,showPeople)
