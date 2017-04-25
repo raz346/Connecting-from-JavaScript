efaf02259b43eb6177209dc132ff8ac5a7928975
Ahmed Al-Hashimi <Alhashimi346@gmail.com>
2017-04-25 20:19:33 +0000

    refactor and create two functions getPeople and showPeople

--- lookup_people.js
+++ lookup_people.js
@@ -1,2 +1,33 @@
-var myArgs = process.argv[2];
-console.log(myArgs);
+const myArgs = process.argv[2];
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+function getPeople (name, done) {
+  client.connect((err) => {
+    if (err) {
+      return console.error("Connection Error", err);
+    }
+    client.query("SELECT * FROM famous_people WHERE  first_name = $1::text" ,[name], (err, result) => {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      done(result.rows);
+      client.end();
+    });
+  });
+}
+function showPeople (rows) {
+console.log(rows);
+  rows.forEach(function(people) {
+  console.log(`found ${people.id} person by the name ${myArgs} \n - ${people.id}: ${people.first_name} ${people.last_name} born in ${people.birthdate} `);
+  });
+}
+getPeople(myArgs,showPeople)
